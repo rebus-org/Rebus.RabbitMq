49793b723d4bf7ffc8002bb8880fa61cd4332697
Mogens Heller Grabe <mookid8000@gmail.com>
2016-09-07 21:49:56 +0200

    try waiting a little

--- Rebus.RabbitMq.Tests/TestRabbitMqAndExplicitRouting.cs
+++ Rebus.RabbitMq.Tests/TestRabbitMqAndExplicitRouting.cs
@@ -58,9 +58,13 @@ namespace Rebus.RabbitMq.Tests
             var queueName = TestConfig.GetName("does_not_exist_yet");
             RabbitMqTransportFactory.DeleteQueue(queueName);
 
+            Thread.Sleep(1000);
+
             // first we send a message to a queue that does not exist at this time
             Console.WriteLine($"Sending 'hej med dig min ven!' message to '{queueName}'");
             await _bus.Advanced.Routing.Send(queueName, "hej med dig min ven!");
+
+            Thread.Sleep(1000);
 
             // then we start a server listening on the queue
             var gotTheMessage = new ManualResetEvent(false);
