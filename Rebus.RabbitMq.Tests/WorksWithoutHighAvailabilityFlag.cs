217922027ac9f1058eb14d3095ddcbfbebde1d28
Mogens Heller Grabe <mookid8000@gmail.com>
2020-04-23 15:45:10 +0200

    ensure it works with empty args

---
+++ Rebus.RabbitMq.Tests/WorksWithoutHighAvailabilityFlag.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rebus.Activation;
+using Rebus.Config;
+using Rebus.Tests.Contracts;
+using Rebus.Tests.Contracts.Extensions;
+// ReSharper disable ArgumentsStyleOther
+// ReSharper disable ArgumentsStyleLiteral
+#pragma warning disable 1998
+
+namespace Rebus.RabbitMq.Tests
+{
+    [TestFixture]
+    public class WorksWithoutHighAvailabilityFlag : FixtureBase
+    {
+        [Test]
+        public async Task CanWorkWithEmptyArguments()
+        {
+            const string connectionString = RabbitMqTransportFactory.ConnectionString;
+
+            var queueName = TestConfig.GetName("empty-args-test");
+
+            Using(new QueueDeleter(queueName));
+
+            var activator = Using(new BuiltinHandlerActivator());
+            var gotTheString = new ManualResetEvent(initialState: false);
+
+            activator.Handle<string>(async str => gotTheString.Set());
+
+            Configure.With(activator)
+                .Transport(t =>
+                {
+                    t.UseRabbitMq(connectionString, queueName)
+                        .InputQueueOptions(q => q.SetArguments(new Dictionary<string, object>()));
+                })
+                .Start();
+
+            var bus = activator.Bus;
+
+            await bus.SendLocal("HEJ HEJ 😘");
+
+            gotTheString.WaitOrDie(timeout: TimeSpan.FromSeconds(2));
+        }
+
+    }
+}
