3fc38bea6b6f8323e334b57c4fc25f067032d724
Mogens Heller Grabe <mookid8000@gmail.com>
2022-02-07 19:54:15 +0100

    fix scripts

---
+++ build.cmd
@@ -0,0 +1,46 @@
+@echo off
+
+set scriptsdir=%~dp0
+set root=%scriptsdir%\..
+set project=%1
+set version=%2
+
+if "%project%"=="" (
+	echo Please invoke the build script with a project name as its first argument.
+	echo.
+	goto exit_fail
+)
+
+if "%version%"=="" (
+	echo Please invoke the build script with a version as its second argument.
+	echo.
+	goto exit_fail
+)
+
+set Version=%version%
+
+pushd %root%
+
+dotnet restore --interactive
+if %ERRORLEVEL% neq 0 (
+	popd
+ 	goto exit_fail
+)
+
+dotnet build "%root%\%project%" -c Release --no-restore
+if %ERRORLEVEL% neq 0 (
+	popd
+ 	goto exit_fail
+)
+
+popd
+
+
+
+
+
+
+goto exit_success
+:exit_fail
+exit /b 1
+:exit_success
---
+++ release.cmd
@@ -0,0 +1,57 @@
+@echo off
+
+set scriptsdir=%~dp0
+set root=%scriptsdir%\..
+set deploydir=%root%\deploy
+set project=%1
+set version=%2
+
+if "%project%"=="" (
+	echo Please invoke the build script with a project name as its first argument.
+	echo.
+	goto exit_fail
+)
+
+if "%version%"=="" (
+	echo Please invoke the build script with a version as its second argument.
+	echo.
+	goto exit_fail
+)
+
+set Version=%version%
+
+if exist "%deploydir%" (
+	rd "%deploydir%" /s/q
+)
+
+pushd %root%
+
+dotnet restore --interactive
+if %ERRORLEVEL% neq 0 (
+	popd
+ 	goto exit_fail
+)
+
+dotnet pack "%root%/%project%" -c Release -o "%deploydir%" -p:PackageVersion=%version% --no-restore
+if %ERRORLEVEL% neq 0 (
+	popd
+ 	goto exit_fail
+)
+
+call scripts\push.cmd "%version%"
+if %ERRORLEVEL% neq 0 (
+	popd
+ 	goto exit_fail
+)
+
+popd
+
+
+
+
+
+
+goto exit_success
+:exit_fail
+exit /b 1
+:exit_success
